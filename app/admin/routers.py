a99a0871626f8dde7447dfa700005c7e18d51940
lengai <371522155@qq.com>
2020-05-28 09:39:12 +0800

    admin 后台首页

--- app/admin/routers.py
+++ app/admin/routers.py
@@ -1,8 +1,11 @@
 from .login.views import LoginView
+from .index.views import IndexView
+
 from flask.blueprints import Blueprint
 
 bp = Blueprint('admin', __name__, url_prefix="/admin")
 bp.add_url_rule('/login', view_func=LoginView.as_view('login'), methods=['get', 'post'])
+bp.add_url_rule('/index', view_func=IndexView.as_view('index'), methods=['get'])
 
 
 def init_app(app):
