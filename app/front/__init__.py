d1805605f2a964f28420fad5b23d9c177879fd98
lengai <371522155@qq.com>
2020-06-05 13:17:19 +0800

    添加 posts 详情api

--- app/front/__init__.py
+++ app/front/__init__.py
@@ -1,4 +1,4 @@
 from app.front.post import Post
 
 def init(api):
-    api.add_resource(Post,'/posts',endpoint="posts")
+    api.add_resource(Post,'/posts',"/posts/<int:id>",endpoint="posts")
--- app/front/post.py
+++ app/front/post.py
@@ -1,20 +1,24 @@
 from app.resources.common import Common
 from flask_restful import marshal, fields
-tags_fields={
-    "id":fields.Integer,
-    "name":fields.String
+
+tags_fields = {
+    "id": fields.Integer,
+    "name": fields.String
 }
 
 posts_fields = {
     "id": fields.Integer,
-    "title":fields.String,
-    "content":fields.String,
-    'tags': fields.List(fields.Nested(tags_fields), attribute='tags')
+    "create_time": fields.String,
+    "title": fields.String,
+    "content": fields.String,
+    'tags': fields.List(fields.Nested(tags_fields), attribute='tags'),
+    'post_views': fields.Integer(attribute="post_views.amount")
 }
 
 
 class Post(Common):
-    def get(self):
+    def get(self,id=None):
         self._fields = posts_fields
-        print(self._service)
-        return self._get()
+        return self._get(_id=id)
+
+
